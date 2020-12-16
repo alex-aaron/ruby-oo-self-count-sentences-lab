f28121f3baa74ed1683a66c52483ca240251ff39
Alex <alexfrancisaaron@gmail.com>
2020-12-16 11:56:59 -0600

    commit tree 04a61091a6168c71bebe47e5f3afebcfad2e71fc

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,42 @@ require 'pry'
 
 class String
 
+
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    delimiters = [".", "?", "!"]
+    sentences_array = self.split(Regexp.union(delimiters))
+    count = sentences_array.length
+    sentences_array.each do |sentences|
+      if sentences == ""
+        count = count - 1
+      else
+        sentences_array.length
+      end
+    end
+    return count
   end
 end
